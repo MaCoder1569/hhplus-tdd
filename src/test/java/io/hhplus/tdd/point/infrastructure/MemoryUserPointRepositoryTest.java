fed22915947299279ce4eae72e2f5a2ac9d20b26
jh <wnsgur1569@naver.com>
2025-11-06 14:54:52 +0900

    repository test 추가

--- src/test/java/io/hhplus/tdd/point/infrastructure/MemoryUserPointRepositoryTest.java
+++ src/test/java/io/hhplus/tdd/point/infrastructure/MemoryUserPointRepositoryTest.java
@@ -10,11 +10,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("사용자 포인트 이력 메모리 리포지토리 테스트")
 class MemoryUserPointRepositoryTest {
-    UserPointTable userPointTable = new UserPointTable();
-    UserPointRepository userPointRepository = new MemoryUserPointRepository(userPointTable);
+    UserPointRepository userPointRepository = new MemoryUserPointRepository(new UserPointTable());
 
     @Test
-    void 미가입자_사용자_조회시_기본값이_생성된다(){
+    void 미가입자_사용자_조회시_기본값_생성(){
         //given & when
         UserPoint userPoint = userPointRepository.findById(1L);
 
@@ -22,5 +21,28 @@ class MemoryUserPointRepositoryTest {
         assertThat(userPoint).isNotNull();
         assertThat((userPoint.id())).isEqualTo(1L);
         assertThat(userPoint.point()).isEqualTo(0);
+    }
+
+    @Test
+    void 사용자_가입() {
+        //given & when
+        UserPoint userPoint = userPointRepository.insertOrUpdate(1L, 100);
+
+        //then
+        assertThat(userPoint.id()).isEqualTo(1L);
+        assertThat(userPoint.point()).isEqualTo(100);
+    }
+
+    @Test
+    void 가입자_사용자_조회() {
+        //given
+        UserPoint newUserPoint = userPointRepository.insertOrUpdate(2L, 100);
+
+        //when
+        UserPoint userPoint = userPointRepository.findById(2L);
+
+        //then
+        assertThat(userPoint.id()).isEqualTo(newUserPoint.id());
+        assertThat(userPoint.point()).isEqualTo(newUserPoint.point());
     }
 }
